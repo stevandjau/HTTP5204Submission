3879dc6cf589339f37dc39433cc462371ca86c04
stevandjau <stevedjau@gmail.com>
2017-01-24 11:18:46 -0500

    assignment 1 finished

---
+++ Assignment1/Assignment1/Controllers/WelcomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        // GET: Welcome
+        public ActionResult Index()
+        {
+            Student s01 = new Student()
+            {
+                name = "Steve",
+                studentNo = "n01151903",
+                program = "Web Development"
+            };
+            return View(s01);
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class Student
+    {
+        public string name { get; set; }
+        public string studentNo { get; set; }
+        public string program { get; set; }
+    }
+}
